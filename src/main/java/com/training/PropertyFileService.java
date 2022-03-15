2a56a09babaddbe23644c5553b7c96d655a0f5a5
V M Aravind <vmaravind07@gmail.com>
2022-03-15 12:09:51 +0530

    Property values mapped to variables and function code added to PropertFileService.java

--- src/main/java/com/training/PropertyFileService.java
+++ src/main/java/com/training/PropertyFileService.java
@@ -1,5 +1,20 @@
 package com.training;
 
-public class PropertyFileService {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
+
+public class PropertyFileService {
+	@Value("{env}")
+	private String env;
+	@Value("{username}")
+	private String username;
+	@Value("{password}")
+	private String password;
+	
+	
+	public String getProperties() {
+		return "env :"+env+", username: "+username;
+	}
 }
