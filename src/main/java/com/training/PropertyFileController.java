7abbeee5aaeffb8db18ebb0c8c748899dc45fec0
V M Aravind <vmaravind07@gmail.com>
2022-03-15 12:29:01 +0530

    function code added to PropertFileController.java

--- src/main/java/com/training/PropertyFileController.java
+++ src/main/java/com/training/PropertyFileController.java
@@ -1,5 +1,15 @@
 package com.training;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class PropertyFileController {
 
+	@Autowired
+	private PropertyFileService pfileService;
+	
+	public String getpropertyFile() {
+		return pfileService.getProperties();
+	}
 }
